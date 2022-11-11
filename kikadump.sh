8e2dbf2585d7423e8415414e2c8eb5c77ac10570
koyu <me@koyu.space>
2022-11-11 18:12:36 +0100

    Disable testing

--- kikadump.sh
+++ kikadump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ffmpeg -y -hide_banner -re -t 10 -i https://kikageohls.akamaized.net/hls/live/2022693/livetvkika_de/master.m3u8 -codec: copy kika.mp4
+ffmpeg -y -hide_banner -re -t 600 -i https://kikageohls.akamaized.net/hls/live/2022693/livetvkika_de/master.m3u8 -codec: copy kika.mp4
 ffmpeg -y -i kika.mp4 -filter:v fps=23 -vf scale=320:-1 comp.mp4
